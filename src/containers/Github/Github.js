47ba6e8ccfc1909ae539ac66c3df9d8325808058
Daniel Zahoran <daniel.zahoran@gmail.com>
2019-02-24 21:09:06 +0100

    Create basic folder structure and initial files for todo-list and github

---
+++ src/containers/Github/Github.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Github extends Component {
+  render() {
+    return <></>
+  }
+}
+
+export default Github;
---
+++ src/containers/TodoList/TodoList.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class TodoList extends Component {
+  render() {
+    return <></>
+  }
+}
+
+export default TodoList;
